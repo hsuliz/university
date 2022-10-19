40ecea043e4d2bbbdd3156f304ab7388864de00b
mymytre <sasha.suliz@gmail.com>
2022-10-19 16:11:04 +0200

    It's data science

--- Year 2023/Python/Week02/main.py
+++ Year 2023/Python/Week02/main.py
@@ -17,6 +17,42 @@ def ex_12(x):
     pass
 
 
+def ex_13(x):
+    print("Exercise 13:")
+    x = x.split(" ")
+    out = 0
+    for i in range(len(x)):
+        out += len(x[i])
+    print(out)
+
+
+def ex_14(x):
+    print("Exercise 14:")
+    x = x.split(" ")
+    print("Longest word:", max(x))
+    print("With size of:", len(max(x)))
+
+
+def ex_15(x):
+    list_of_number = [1, 2, 3]
+    pass
+
+
+def ex_16():
+    gvr = "GvR"
+    _gvr = "Guido van Rossum"
+    gvr = gvr.split(" ")
+    _gvr = _gvr.split(" ")
+
+    for i in range(3):
+        gvr[i] = _gvr[i]
+
+    print(gvr)
+
+
 if __name__ == '__main__':
     ex_10(line)
     ex_11(word, "a")
+    ex_13(line)
+    ex_14(line)
+    ex_16()
