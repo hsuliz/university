0b218b1b8ae50d826ecc9b365ec28a02c5f6d30a
mymytre <sasha.suliz@gmail.com>
2022-10-19 12:45:21 +0200

    a bit of python

--- Year 2023/Python/Week02/main.py
+++ Year 2023/Python/Week02/main.py
@@ -1,1 +1,22 @@
-print(2)
+word = "abcdefg"
+line = "roses are red violets are blue"
+
+
+def ex_10(x):
+    print("Exercise 10:")
+    print(len(x.split(" ")))
+
+
+def ex_11(x, y):
+    print("Exercise 11:")
+    print('_'.join(map(str, y)))
+    print('_'.join(map(str, x)))
+
+
+def ex_12(x):
+    pass
+
+
+if __name__ == '__main__':
+    ex_10(line)
+    ex_11(word, "a")
