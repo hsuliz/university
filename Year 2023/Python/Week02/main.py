9d103f8b524fa259b955534138694cbddec4f0bd
mymytre <sasha.suliz@gmail.com>
2022-10-25 18:37:24 +0200

    Python week 2 done

--- Year 2023/Python/Week02/main.py
+++ Year 2023/Python/Week02/main.py
@@ -1,5 +1,5 @@
 word = "abcdefg"
-line = "roses are red violets are blue"
+line = """Roses are red violets are blue"""
 
 
 def ex_10(x):
@@ -14,7 +14,11 @@ def ex_11(x, y):
 
 
 def ex_12(x):
-    pass
+    print("Exercise 12:")
+    a = "".join(x[0] for x in x.split())
+    b = "".join(x[-1] for x in x.split())
+    print(a)
+    print(b)
 
 
 def ex_13(x):
@@ -33,26 +37,55 @@ def ex_14(x):
     print("With size of:", len(max(x)))
 
 
-def ex_15(x):
-    list_of_number = [1, 2, 3]
-    pass
+def ex_15():
+    print("Exercise 15:")
+    list_of_number = [i + 1 for i in range(5)]
+    out_string = "".join(str(n) for n in list_of_number)
+    print(out_string)
 
 
 def ex_16():
-    gvr = "GvR"
-    _gvr = "Guido van Rossum"
-    gvr = gvr.split(" ")
-    _gvr = _gvr.split(" ")
+    print("Exercise 16:")
+    in_string = "W tekście znajdującym się w zmiennej line zamienić ciąg znaków GvR na Guido van Rossum."
+    out_string = in_string.replace("GvR", "Guido van Rossum")
+    print(out_string)
 
-    for i in range(3):
-        gvr[i] = _gvr[i]
 
-    print(gvr)
+def ex_17():
+    print("Exercise 17:")
+    x = line.split(" ")
+    for i in range(len(x)):
+        x[i] = x[i].lower()
+    by_alph = sorted(x)
+    print(by_alph)
+    by_len = sorted(x, key=len)
+    print(by_len)
+
+
+def ex_18():
+    print("Exercise 18:")
+    # must be 21
+    x = 1000000000000000000011100
+    y = str(x)
+    print(y.count('0'))
+
+
+def ex_19():
+    print("Exercise 19:")
+    arr = [1, 2, 33, 44, 555, 666]
+
+    for i in range(len(arr)):
+        print(str(arr[i]).zfill(3))
 
 
 if __name__ == '__main__':
     ex_10(line)
     ex_11(word, "a")
+    ex_12(line)
     ex_13(line)
     ex_14(line)
+    ex_15()
     ex_16()
+    ex_17()
+    ex_18()
+    ex_19()
