350eab61b8962d791b6fc84cf9c81af9fa901d6a
mymytre <sasha.suliz@gmail.com>
2022-10-27 00:06:18 +0200

    Started a js

--- Year 2023/WWW Techniques/TouhouProject/main.js
+++ Year 2023/WWW Techniques/TouhouProject/main.js
@@ -1,1 +1,8 @@
-console.log("Hi, im a cute javascript")
+function loadDoc() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {
+        document.getElementById("demo").innerHTML = this.responseText;
+    }
+    xhttp.open("GET", "https://api.github.com", true);
+    xhttp.send();
+}
