5571309a5a9521713bd6301432e0e004909afa31
mymytre <sasha.suliz@gmail.com>
2022-12-16 13:44:50 +0100

    Color of navbar

--- year-2023/www-techniques/meat-in-bread/front/src/App.tsx
+++ year-2023/www-techniques/meat-in-bread/front/src/App.tsx
@@ -6,31 +6,40 @@ import './App.css';
 import UserInfo from "./component/user-info";
 import MenuComponent from "./component/menu-component";
 import Navbar from "react-bootstrap/Navbar";
-import {NavItem} from "react-bootstrap";
+import {Nav, NavItem} from "react-bootstrap";
 
 
 const App: React.FC = () => {
 
     return (
-        <div>
+        <>
             <Navbar className="color-nav">
                 <Container>
+                    <Navbar.Brand>
+                        Nigga
+                    </Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Link to={"/menu"} className="nav-link">Menu</Link>
+                        <Link to={"/menu"} className="nav-link">User</Link>
+                    </Nav>
                     <NavItem>
-                        <Link to={"/menu"} className="nav-link">
-                            Menu
+                        <Link to={"/menu"} className="nav-link accordion-button">
+                            Log in
                         </Link>
-
+                        <Link to={"/menu"} className="nav-link accordion-button">
+                            Sign up
+                        </Link>
                     </NavItem>
                 </Container>
             </Navbar>
             <Container>
                 <Routes>
                     <Route path='/home' element={<UserInfo/>}></Route>
-                    <Route path='/login' element={< SignupComponent/>}></Route>
                     <Route path='/menu' element={<MenuComponent/>}></Route>
+                    <Route path='/login' element={< SignupComponent/>}></Route>
                 </Routes>
             </Container>
-        </div>
+        </>
     );
 };
 
