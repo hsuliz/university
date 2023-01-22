6a8f3b91858b6ff81b3ac6c52b17deea19c95a8c
mymytre <sasha.suliz@gmail.com>
2023-01-22 18:40:28 +0100

    python -> added printing

--- year-2023/python/final-project/prim_algo.py
+++ year-2023/python/final-project/prim_algo.py
@@ -6,6 +6,7 @@ from parser import MatrixParser
 class PrimAlgorithm:
 
     def __init__(self, matrix=None):
+        self.out_matrix = None
         if matrix is None:
             matrix = []
         self._G = matrix
@@ -20,7 +21,7 @@ class PrimAlgorithm:
     def calc(self, to_file=None):
         if len(self._G) is 0:
             raise ValueError("Matrix is empty!!")
-        out_matrix = [[0 for _ in range(len(self._G))] for _ in range(len(self._G))]
+        self.out_matrix = [[0 for _ in range(len(self._G))] for _ in range(len(self._G))]
         selected = [False] * len(self._G)
         selected[0] = True
         edge_quantity = 0
@@ -37,10 +38,23 @@ class PrimAlgorithm:
                                 minimum = self._G[i][j]
                                 v = i
                                 w = j
-            out_matrix[v][w] = self._G[v][w]
-            out_matrix[w][v] = self._G[v][w]
+            self.out_matrix[v][w] = self._G[v][w]
+            self.out_matrix[w][v] = self._G[v][w]
             selected[w] = True
             edge_quantity += 1
         if to_file is not None:
-            MatrixParser.write_to_file(out_matrix, to_file)
-        return out_matrix
+            MatrixParser.write_to_file(self.out_matrix, to_file)
+        return self.out_matrix
+
+    def print_mst_matrix(self):
+        self.__print_as_matrix(self.out_matrix)
+
+    def print_calc_matrix(self):
+        self.__print_as_matrix(self._G)
+
+    @staticmethod
+    def __print_as_matrix(matrix):
+        for line in matrix:
+            for number in line:
+                print(number, end='  ')
+            print()
