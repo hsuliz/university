f7ce4bfc0150a0ce64596903bb0d5fc8a30b6080
mymytre <sasha.suliz@gmail.com>
2023-05-14 19:14:27 +0200

    fortran -> 41

--- year-2023/fortran/assignment-08/p40.f90
+++ year-2023/fortran/assignment-08/p40.f90
@@ -1,7 +1,7 @@
-PROGRAM prog40
+PROGRAM p40
     USE moje_procedury_i_funkcje; IMPLICIT NONE
     REAL :: fahr
-    CALL Input(fahr) !wywolanie procedury "Input", jej definicja jest udostepniona prez "USE ! nazwa modulu"
-    CALL Output(fahr, Temp_C(fahr)) ! wywolanie procedury "Output", drugim argumentem ! aktualnym jest wartosc zwracana prez funkcje "Temp_C" - parametrami procedur moga byc funkcje !!!
+    CALL Input(fahr)
+    CALL Output(fahr, Temp_C(fahr))
     STOP;
-END PROGRAM prog40
+END PROGRAM p40
