a5613c0f7345f9fb127abb4fc59a365aca6b9a89
hsuliz <sasha.suliz@gmail.com>
2024-01-27 10:33:42 +0100

    DAPP: assignment 04 Init

--- year-2023/diffuse-and-parallel-programming/assignment_04/compileAndRun.sh
+++ year-2023/diffuse-and-parallel-programming/assignment_04/compileAndRun.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-g++-13 -fopenmp -O2 DataSupplier.cpp Force.cpp main.cpp MyForce.cpp SimpleDataSupplier.cpp Simulation.cpp && time ./a.out
+g++-13 -fopenmp -O2 DataSupplier.cpp Force.cpp main.cpp MyForce.cpp SimpleDataSupplier.cpp Simulation1.cpp && ./a.out
